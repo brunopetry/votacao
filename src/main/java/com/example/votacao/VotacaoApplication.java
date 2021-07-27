1602f7332033bbe623d60a17d266a0cad29e6bc6
Bruno Petry <bruno.petry@fornecedores.sicoob.com.br>
2021-07-27 18:40:10 -0300

    config database

--- src/main/java/com/example/votacao/VotacaoApplication.java
+++ src/main/java/com/example/votacao/VotacaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class VotacaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(VotacaoApplication.class, args);
 	}
 
 }
