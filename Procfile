18ca9c7fe682896f1eb64ad12e4cf35ebd21b5f8
Bruno Petry <bruno.petry@fornecedores.sicoob.com.br>
2021-07-29 20:15:05 -0300

    configs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/votacao-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/votacao/VotacaoApplication.java
+++ src/main/java/com/example/votacao/VotacaoApplication.java
@@ -1,10 +1,21 @@
 package com.example.votacao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class VotacaoApplication {
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Bem vindo ao sistema de votação!";
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotacaoApplication.class, args);
