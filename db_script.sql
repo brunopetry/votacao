3828e6a3d315c6087c42f5f8fdc1bdb1ef628ae4
Bruno Petry <bruno.petry@fornecedores.sicoob.com.br>
2021-08-02 16:03:35 -0300

    atualizando script bd

--- db_script.sql
+++ db_script.sql
@@ -5,13 +5,16 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema s7gieufzb379zhg8
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema s7gieufzb379zhg8
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `s7gieufzb379zhg8` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `s7gieufzb379zhg8` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `s7gieufzb379zhg8` ;
 
 -- -----------------------------------------------------
@@ -23,30 +26,35 @@ CREATE TABLE IF NOT EXISTS `s7gieufzb379zhg8`.`pauta` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `dataCriacao` TIMESTAMP NOT NULL,
-  `aberta` TINYINT NOT NULL DEFAULT 0,
-  `dataAbertura` TIMESTAMP NOT NULL,
-  `dataFechamento` TIMESTAMP NOT NULL,
+  `dataAbertura` TIMESTAMP NULL DEFAULT NULL,
+  `dataFechamento` TIMESTAMP NULL DEFAULT NULL,
+  `qtdVotosNao` INT NULL DEFAULT NULL,
+  `qtdVotosSim` INT NULL DEFAULT NULL,
+  `resultado` VARCHAR(6) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `s7gieufzb379zhg8`.`Voto`
+-- Table `s7gieufzb379zhg8`.`voto`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `s7gieufzb379zhg8`.`Voto` ;
+DROP TABLE IF EXISTS `s7gieufzb379zhg8`.`voto` ;
 
-CREATE TABLE IF NOT EXISTS `s7gieufzb379zhg8`.`Voto` (
+CREATE TABLE IF NOT EXISTS `s7gieufzb379zhg8`.`voto` (
   `cpf` VARCHAR(11) NOT NULL,
   `voto` ENUM('SIM', 'NÃO') NOT NULL,
   `pauta_id` INT NOT NULL,
-  PRIMARY KEY (`cpf`),
+  PRIMARY KEY USING BTREE (`cpf`, `pauta_id`),
   INDEX `fk_Voto_pauta_idx` (`pauta_id` ASC) VISIBLE,
   CONSTRAINT `fk_Voto_pauta`
     FOREIGN KEY (`pauta_id`)
-    REFERENCES `s7gieufzb379zhg8`.`pauta` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `s7gieufzb379zhg8`.`pauta` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
